d09e5aefeebdc73a02f44b6ec2e51a7eb503f4d6
jjlakin <j.lakin@gmail.com>
2014-12-19 15:54:35 +0000

    inject

--- lib/array_method.rb
+++ lib/array_method.rb
@@ -1,10 +1,64 @@
 class Array
 
-	def my_push(push_object)
-		array_count = self.count
-		self[array_count] = push_object
-		self
-	end
+  def my_push(push_object)
+    array_count = self.count
+    self[array_count] = push_object
+    self
+  end
 
+  def my_pop
+    array_count = self.count - 1
+    self.delete_at(array_count)
+  end
+
+  def my_shift
+    self.delete_at(0)
+  end
+
+  def my_slice(slice_point)
+    self[slice_point]
+  end
+
+  def my_slice!(slice_point)
+    new_array = self[slice_point]
+    self - new_array
+    new_array
+  end
+
+  def my_inject(v = nil)
+    i = 0
+    return 'wrong class' if self.class != Array
+    while self[i]
+      (v = self[0]; i +=1) if v == nil 
+      v = yield v, self[i]
+      i += 1 
+    end
+    
+    return v
+
+    # v = yield v, self[i]
+    # i += 1 
+    # v = yield v, self[i]
+    # i += 1 
+    # v
+    # v = yield v, self[i]
+    # p v, i
+
+
+
+    # copy = self.dup
+    # v ==
+    # while self[]
+    
+  end
+
+    
+  
+
+  # def my_each
+  #   self.index = length
+  #   yield
+  #   my_each[]
+  # end
 
 end
--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -4,10 +4,64 @@ require 'array_method'
 
 describe Array do
 
+	let (:arr) {Array.new[5] = [1, 3, 5, 6, 2]}
+
 	it 'should push the value into the array' do
 
-		arr = [1, 3, 5, 6, 2]
+		
 		expect(arr.my_push(4)).to eq( [1, 3, 5, 6, 2, 4] )
+
+	end
+
+	it 'should pop the end value of an array' do
+		
+		
+		expect(arr.my_pop).to eq(2)
+		expect(arr).to eq( [1, 3, 5, 6] )
+
+	end
+
+	it 'should shift the array' do
+		expect(arr.my_shift).to eq(1)
+		expect(arr).to eq( [3, 5, 6, 2] )
+	end
+
+	context 'slice' do
+
+		it 'should take an item out of the array' do
+			expect(arr.my_slice(1)).to eq(3)
+		end
+		
+		it 'should output item out of the array' do
+			expect(arr.my_slice(1..3)).to eq([3, 5, 6])
+		end
+
+		it 'should take an item out of the array' do
+			expect(arr.my_slice!(1..3)).to eq([3, 5, 6])
+			# expect(arr).to eq([1, 2])
+		end
+
+	end
+
+	context 'inject' do
+
+		it 'should inject' do
+			array = [1,2,3,4]
+			expect(array.inject {|memo, v| memo + v}).to eq(10)
+			expect(array.my_inject {|memo, v| memo + v}).to eq(10)
+		end	
+		
+		it 'should inject with multiplication' do
+			array = [1,2,3,4]
+			expect(array.inject {|memo, v| memo * v}).to eq(24)
+			expect(array.my_inject {|memo, v| memo * v}).to eq(24)
+		end	
+
+		it 'should inject with argument' do
+			array = [1,2,3,4]
+			expect(array.inject(10) {|memo, v| memo + v}).to eq(20)
+			expect(array.my_inject(10) {|memo, v| memo + v}).to eq(20)
+		end	
 
 	end
 
